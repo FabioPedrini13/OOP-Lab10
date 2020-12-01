f8bf8619cb9aa36d9496fcba5279d2703a1b59b0
Fabio Pedrini - fabio.pedrini3@studio.unibo.it <fabio.pedrini3@studio.unibo.it>
2020-12-01 16:47:07 +0100

    Fixed error

--- src/it/unibo/oop/lab/reactivegui01/ConcurrentGUI.java
+++ src/it/unibo/oop/lab/reactivegui01/ConcurrentGUI.java
@@ -88,7 +88,7 @@ public final class ConcurrentGUI extends JFrame {
                      * All the operations on the GUI must be performed by the
                      * Event-Dispatch Thread (EDT)!
                      */
-                    SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(Integer.toString(Agent.this.counter)));
+                    SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(Integer.toString(this.counter)));
                     this.counter++;
                     Thread.sleep(100);
                 } catch (InvocationTargetException | InterruptedException ex) {
